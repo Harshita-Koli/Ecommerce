521cabafe4a9df3282bcdb21f661003eb4adff77
Your Name <you@example.com>
2023-12-07 00:02:52 -0800

    write sort before Direction

--- src/main/java/electronicStore/services/UserService.java
+++ src/main/java/electronicStore/services/UserService.java
@@ -11,7 +11,7 @@ public interface UserService {
    UserDto createUser(UserDto userDto);
    UserDto updateUser(UserDto userDto, String userId);
    UserDto getUserById(String userId);
-   pagebleResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String direction);
+   pagebleResponse getAllUsers(Integer pageNumber, Integer pageSize, String sortBy, String sortDirection);
    void deleteUser(String userId);
    UserDto getUserByEmail(String email);
    List<UserDto> searchUser(String keyword);
