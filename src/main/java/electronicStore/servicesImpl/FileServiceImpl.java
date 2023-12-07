77c9350c4d28d40e85522e1571b6fd154ab2cd94
Your Name <you@example.com>
2023-12-07 00:04:09 -0800

    import File

--- src/main/java/electronicStore/servicesImpl/FileServiceImpl.java
+++ src/main/java/electronicStore/servicesImpl/FileServiceImpl.java
@@ -7,9 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,12 +24,12 @@ public class FileServiceImpl implements FileService {
         String filename = UUID.randomUUID().toString();
         String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
         String fileNameWithExtension = filename + extension;
-        String fullPathWithFileName = path  + fileNameWithExtension;
-logger.info("full image path:{}",fullPathWithFileName);
+        String fullPathWithFileName = path + fileNameWithExtension;
+        logger.info("full image path:{}", fullPathWithFileName);
         if (extension.equalsIgnoreCase(".png") || extension.equalsIgnoreCase(".jpg") || extension.equalsIgnoreCase(".jpeg")) {
 
             // File save
-            logger.info("file extension is {}",extension);
+            logger.info("file extension is {}", extension);
             File folder = new File(path);
             if (!folder.exists()) {
                 //create the foulder
@@ -47,7 +45,10 @@ logger.info("full image path:{}",fullPathWithFileName);
     }
 
     @Override
-    public InputStream getResource(String path, String name) {
-        return null;
+    public InputStream getResource(String path, String name) throws FileNotFoundException {
+        String fullPath = path + File.separator + name;
+        InputStream inputStream = new FileInputStream(fullPath);
+
+        return inputStream;
     }
 }
